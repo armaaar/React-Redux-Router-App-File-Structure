f89f9ece4f75382e4d83237ca4274133c058aa44
Ahmed Rafik Radwan <ahmedrafik_maaar@outlook.com>
2019-12-30 13:28:43 +0200

    Add React Meta Tags

--- src/routes/home/home.js
+++ src/routes/home/home.js
@@ -1,6 +1,14 @@
 import React from 'react'
 import App from '../../components/app'
+import MetaTags from 'react-meta-tags';
 
 export default function Home () {
-    return <App />
+    return (
+        <div>
+        <MetaTags>
+            <title>Cool React App</title>
+        </MetaTags>
+            <App />
+        </div>
+    )
 }
--- src/routes/route-switcher.js
+++ src/routes/route-switcher.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { Switch, Route } from 'react-router-dom'
 import loadable from '@loadable/component'
+import MetaTags from 'react-meta-tags';
 
 const Home = loadable(() => import('./home'))
 
@@ -12,6 +13,9 @@ export default class RouteSwitcher extends Component {
                     <Home />
                 </Route>
                 <Route path="*">
+                    <MetaTags>
+                        <title>404 Not Found</title>
+                    </MetaTags>
                     <h1>There Is No Match.</h1>
                 </Route>
             </Switch>
